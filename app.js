14bb884347a6b6a685af58a9d2cc378962103c9d
GavrilovaMilena <mia.gggg@yandex.ru>
2021-06-27 18:25:44 +0300

    cors

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const cors = require('cors');
 
 const DB_ADDRESS = require('./utils/config');
 const limiter = require('./utils/rateLimit');
@@ -15,20 +14,6 @@ const errorRouter = require('./routes/error');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
-
-const options = {
-  origin: [
-    'http://localhost:3001',
-    'https://alligator.nomoredomains.icu/'
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept'],
-  credentials: true,
-};
-
-app.use('*', cors(options));
 
 // Слушаем 3000 порт
 const { PORT = 3000, DB_LOCAL = DB_ADDRESS } = process.env;
